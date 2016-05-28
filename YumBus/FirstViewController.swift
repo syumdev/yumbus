21011d0668b8e821e39435a6b58e161554640691
Seungheon Yum <syumdev@gmail.com>
2016-05-28 14:26:59 -0400

    first commit

---
+++ YumBus/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  YumBus
+//
+//  Created by Seungheon Yum on 5/28/16.
+//  Copyright © 2016 syumdev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
